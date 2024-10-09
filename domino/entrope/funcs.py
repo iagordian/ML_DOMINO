59aaa2838f3a177ed10660508784a983aa2fa05b
Игорь Гордиан <114696975+iagordian@users.noreply.github.com>
2024-10-09 16:02:42 +0300

    Add files via upload

--- domino/entrope/funcs.py
+++ domino/entrope/funcs.py
@@ -1,38 +1,40 @@
 
 import numpy as np
-from typing import List
+from typing import List, Optional
+from functools import lru_cache
 
-def get_entrope(data: List[int]) -> float:
+@lru_cache
+def caculate_entrope(*args, total=None) -> float:
+  '''Возвращает значение энтропии для переданных элементов'''
+
+  if total is None:
+    total = sum(args)
+
+  if total == 0:
+     return 0
+
+  args = map(lambda arg: arg / total, args)
+  result = sum(arg * np.log2(to_log(arg)) for arg in args)
+  return -result
+
+def to_log(val):
+   return val or 1
+
+def get_entrope(data: np.ndarray) -> float:
     '''Возвращает значение энтропии для переданного массива'''
 
-    unique = np.unique(data)
-    length = len(data)
+    elems = np.unique(data)
+    elems_cnt = [np.sum(data==el) for el in elems]
+    return caculate_entrope(*elems_cnt, total=len(data))
 
-    entrope = 0
-    for el in unique:
-        freack = np.sum(data==el) / length
-        entrope += freack * np.log2(freack)
+def entrope_change(data):
+    return get_entrope(data) - get_entrope(data[:-1])
 
-    return -entrope
-
-def get_secondary_growth_entrope(entrope: float, data: List[int]) -> float:
+def get_secondary_growth_entrope(data: np.ndarray, entrope: Optional[float] = None) -> float:
     '''Возвращает прирост к энтропии при делении массива на 2'''
+    if entrope is None:
+        entrope = get_entrope(data)
     center = len(data) // 2
     return entrope - 0.5 * (get_entrope(data[:center]) + get_entrope(data[center:]))
 
-def get_ternary_growth_entrope(entrope: float, data: List[int]) -> float:
-    '''Возвращает прирост к энтропии при делении массива на 3'''    
-    third = len(data) // 3
-    return entrope - (1 / 3) * (get_entrope(data[:third]) + get_entrope(data[third:third * 2]) + get_entrope(data[third * 2:]))
 
-def get_secondary_growth_entrope_full(data: List[int]) -> float:
-    '''Возвращает прирост к энтропии при делении массива на 2'''
-    entrope = get_entrope(data)
-    center = len(data) // 2
-    return entrope - 0.5 * (get_entrope(data[:center]) + get_entrope(data[center:]))
-
-def get_ternary_growth_entrope_full(data: List[int]) -> float:
-    '''Возвращает прирост к энтропии при делении массива на 3'''    
-    entrope = get_entrope(data)
-    third = len(data) // 3
-    return entrope - (1 / 3) * (get_entrope(data[:third]) + get_entrope(data[third:third * 2]) + get_entrope(data[third * 2:]))
